1a37f9f76cefe04fae13c51076e4624d689f228b
Chia Hsin Wu <gill861111@gmail.com>
2020-05-25 21:31:01 +0800

    Add Index's ViewModels

---
+++ SurvivalGame/ViewModels/Index/PopularViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurvivalGame.ViewModels.Index
+{
+    public class PopularViewModel
+    {
+        public string PopularName { get; set; }
+        public int PopularPrice { get; set; }
+        public string PopularImg { get; set; }
+    }
+}
